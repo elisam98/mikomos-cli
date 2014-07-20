984eb43b244413fc7f395d9b38ea57a4ab35659e
elisam98 on Ubuntu <cornbeefonrye@hotmail.com>
2014-07-19 22:06:03 -0400

    removed filter option from application route

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,8 +3,5 @@ export default Ember.Controller.extend({
 	isHome: function() {
 		var path = this.get('currentPath');
 		return path === 'index';
-	}.property('currentPath'),
-	needsFilter: function() {
-		return this.get('currentPath') === 'search.index';
 	}.property('currentPath')
 });
